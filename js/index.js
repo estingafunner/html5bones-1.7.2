4c60e7102be1a4b9dbfc182fe0736afa07556d2b
Marcus Larson <38468418+estingafunner@users.noreply.github.com>
2022-03-27 21:57:14 -0700

    Commit Message!

--- js/index.js
+++ js/index.js
@@ -10,94 +10,38 @@ import fetch, {
   Request,
   Response,
 } from 'node-fetch'
-//import fetch from 'node-fetch';
 
-//global.fetch = fetch;
-//global.Headers = fetch.Headers;
+global.fetch = fetch;
+global.Headers = fetch.Headers;
 
 
-const https = require('https');
-var fs = require('fs');
-//var fetch = require('node-fetch');
+//let nowish = (new Date().getTime()) - 3;
 
-let nowish = (new Date().getTime()) - 3;
-
+function Tesi() {
 const id = 'DCHBKYZF5NMXHCV8AG4M1J53DFDONO8Z';
 const secret = 'WV0KBNNCRLP0SO3CYZMOGFQATYTPG2Y';
 
-//let fetch = require("node-fetch");
 
-let myHeaders = new fetch.Headers();
+let myHeaders = new Headers();
 myHeaders.append("Authorization", "Basic e3thcGlVTn19Ont7YXBpUGFzc319");
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-fetch("HTTPS:////api.limblecmms.com:443/v2/tasks/?tasks=5&status=0", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-///////////////////
-
-/* const options = {
+const requestOptions = {
   'method': 'GET',
-  'hostname': 'api.limblecmms.com',
-  'path': '/v2/tasks/?tasks=5&status=1',
+  redirect: 'follow',
+  //'hostname': 'api.limblecmms.com',
+  //'path': '/v2/tasks/?tasks=5&status=0',
   'headers': {
     'Authorization': 'Basic ' + Buffer.from(id + ':' + secret).toString('base64')
   }
 };
 
-var req = https.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-
-    res.on("end", function (chunk) {
-      var body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
-
-    res.on("error", function (error) {
-      console.error(error);
-    });
-  });
-
-  req.end(); */
 
 
-
-  /////
-
-/* const req = https.request(options, function (res) {
-  const chunks = [];
-
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
+fetch("HTTPS:////api.limblecmms.com:443/v2/tasks/?tasks=5&status=0", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
 
 
-  res.on("end", function (chunk) {
-    const body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-
-
-  res.on("error", function (error) {
-    console.error(error);
-  });
-});
-
-
-
-
-
-
-req.end(); */
+/////////
