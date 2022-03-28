00dedb7f447c66fd455be96b779cebfde852d1f2
Marcus Larson <38468418+estingafunner@users.noreply.github.com>
2022-03-27 22:27:57 -0700

    commit message

--- js/index.js
+++ js/index.js
@@ -14,13 +14,11 @@ import fetch, {
 global.fetch = fetch;
 global.Headers = fetch.Headers;
 
-
 //let nowish = (new Date().getTime()) - 3;
 
 function Tesi() {
 const id = 'DCHBKYZF5NMXHCV8AG4M1J53DFDONO8Z';
 const secret = 'WV0KBNNCRLP0SO3CYZMOGFQATYTPG2Y';
-
 
 let myHeaders = new Headers();
 myHeaders.append("Authorization", "Basic e3thcGlVTn19Ont7YXBpUGFzc319");
@@ -35,8 +33,6 @@ const requestOptions = {
   }
 };
 
-
-
 fetch("HTTPS:////api.limblecmms.com:443/v2/tasks/?tasks=5&status=0", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
@@ -45,3 +41,10 @@ fetch("HTTPS:////api.limblecmms.com:443/v2/tasks/?tasks=5&status=0", requestOpti
 
 
 /////////
+/*
+document.getElementById("bPull").onclick = Tesi();
+ 0
+
+
+
+*/
