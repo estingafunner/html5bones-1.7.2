3c6d18d2ee2d6c2f3e07c563caa7e4a097d8c769
Marcus Larson <38468418+estingafunner@users.noreply.github.com>
2022-04-16 04:54:31 -0700

    Errors Cleared! No Clue What To Do Next!

---
+++ index.js
@@ -0,0 +1,54 @@
+import fetch, {
+  Blob,
+  blobFrom,
+  blobFromSync,
+  File,
+  fileFrom,
+  fileFromSync,
+  FormData,
+  Headers,
+  Request,
+  Response,
+} from 'node-fetch'
+
+global.fetch = fetch;
+global.Headers = fetch.Headers;
+
+let nowish = (new Date().getTime());
+console.log(nowish);
+
+//function Tesi() {
+const id = 'DCHBKYZF5NMXHCV8AG4M1J53DFDONO8Z';
+const secret = 'WV0KBNNCRLP0SO3CYZMOGFQATYTPG2Y';
+
+let myHeaders = new Headers();
+myHeaders.append("Authorization", "Basic e3thcGlVTn19Ont7YXBpUGFzc319");
+
+const requestOptions = {
+  'method': 'GET',
+  redirect: 'follow',
+  //'hostname': 'api.limblecmms.com',
+  //'path': '/v2/tasks/?tasks=5&status=0',
+  'headers': {
+    'Authorization': 'Basic ' + Buffer.from(id + ':' + secret).toString('base64')
+  }
+};
+
+fetch("HTTPS:////api.limblecmms.com:443/v2/tasks/?limit=5", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+
+
+
+
+/////////
+/*
+document.getElementById("bPull").onclick = Tesi();
+ 0
+
+
+
+*/
